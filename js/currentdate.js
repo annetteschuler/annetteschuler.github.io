1826176b1593e7b1d5f79c4a2d07160c8b96eac9
annetteschuler.github.io <annetteschuler1964@gmail.com>
2021-01-08 22:42:01 -0600

    save

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,3 +1,34 @@
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-// document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
-document.getElementById("currentdate").textContent = new Date().toLocaleDateString('en-US', options);
+// day names array
+ const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+// long month names array
+ const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+    const todaysdate = new Date();
+    const dayName = dayNames[todaysdate.getDay()];
+    const monthName = months[todaysdate.getMonth()];
+    const currentdate = dayName + "," + todaysdate.getDate() + " " + monthName + "," + todaysdate.getFullYear();
+
+    document.getElementById('currentdate').textContent = currentdate;
+]    
+]
