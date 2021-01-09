573b6c6eee95e52f92eb9992f848d9d40f70cc12
annetteschuler.github.io <annetteschuler1964@gmail.com>
2021-01-09 00:26:24 -0600

    save

--- js/currentdate.js
+++ js/currentdate.js
@@ -27,6 +27,9 @@
     const todaysdate = new Date();
     const dayName = dayNames[todaysdate.getDay()];
     const monthName = months[todaysdate.getMonth()];
-    const currentdate = dayName + "," + todaysdate.getDate() + " " + monthName + "," + todaysdate.getFullYear();
+    const hours = hours[getHours()]; 
+    const minute = minutes[getMinutes()];
+    const seconds = seconds[getSeconds()];
+    const currentdate = dayName + ", " + monthName + " " + todaysdate.getDate() + ", " + todaysdate.getFullYear() + " at " + hours + ":" minutes +":" seconds;
 
     document.getElementById('currentdate').textContent = currentdate;
