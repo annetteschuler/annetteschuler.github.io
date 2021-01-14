e420cb13f22189a321e2263cb292b06909a0432b
annetteschuler.github.io <annetteschuler1964@gmail.com>
2021-01-13 19:27:11 -0600

    aa

--- js/datelastmodified.js
+++ js/datelastmodified.js
@@ -1,10 +1,9 @@
 // date last modified
-var string = document.lastModified;
+// var string = document.dateLastModified;
 
-alert(document.lastModified);
+// alert(document.lastModified);
 //returns: Tuesday, December 16, 2017 11:09:42
 
-let oLastModif = new Date(document.lastModified);
-let nLastModif = Date.parse(document.lastModified);
+let lastModified = new Date(document.lastModified);
 
     document.getElementById('lastModified').textContent = lastModified;
