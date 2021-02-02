6dcdb54ec44aa30fa461947858b5d9bf9864417a
annetteschuler.github.io <annetteschuler1964@gmail.com>
2021-02-01 23:08:36 -0600

    save

---
+++ lesson5/lesson5.js/currentdate.js
@@ -0,0 +1,35 @@
+function currentDate() {
+    // day names array
+ const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+// long month names array
+ const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+    const todaysdate = new Date();
+    const dayName = dayNames[todaysdate.getDay()];
+    const monthName = months[todaysdate.getMonth()];
+    const currentdate = dayName + ", " + monthName + " " + todaysdate.getDate() + ", " + todaysdate.getFullYear();
+    
+    
+    document.getElementById("currentDate").textContent = currentdate;
+}
