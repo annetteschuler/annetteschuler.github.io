f11c42efcf4de5aee41d6c22b4cf01ec49ea72af
annetteschuler.github.io <annetteschuler1964@gmail.com>
2021-02-02 19:34:02 -0600

    save

---
+++ lesson5/lesson5.js/banner.js
@@ -0,0 +1,19 @@
+function banner () {
+    var weekday - new Array (7);
+    weekday[0]=  "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var day = weekday[now.getDay()];
+
+    if (day == "Friday") {
+        document.writeIn("<h2>Banner</h2>");
+    }
+    else (hide);
+  }
+
+ //use display: none if not on Friday 
+}
