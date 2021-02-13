7d6c028627627f41ea19e029b5fc0a1a283bf30f
annetteschuler.github.io <annetteschuler1964@gmail.com>
2021-02-12 21:16:31 -0600

    save

---
+++ lesson6/lesson6.js/windchill.js
@@ -0,0 +1,17 @@
+function wChill() {
+    let temp=parseFloat(document.querySelector('#temp').textContent);
+    let windspeed=(document.querySelector('#speed').textContent);
+    let windchill='N/A';
+
+    if (&& temp){
+        windchill =- windchill(temp, windspeed); 
+    }
+
+    if () {
+        document.querySelector('#windchill').textContent=-windChill(temp, windspeed);
+        
+    }
+
+    const div-document.querySelectorAll('div');
+    let temp-document.querySelector('#temp');
+}
