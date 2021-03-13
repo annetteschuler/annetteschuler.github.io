17220f430e17742adab8b32822b2ce7908663b2e
annetteschuler.github.io <annetteschuler1964@gmail.com>
2021-03-12 22:09:04 -0600

    save

--- lesson10/lesson10.js/forecast1.js
+++ lesson10/lesson10.js/forecast1.js
@@ -3,32 +3,35 @@ const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
+     
 
        // console.log(jsObject);    this was used to test the url and it is good
-    //    let currenttemp = document.createElement('p');
+
+       const temp = Number(jsObject.main.temp);
+       const windspeed = Number(jsObject.wind.speed);
+       const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+      
+    
+       //    let currenttemp = document.createElement('p');
        document.getElementById('current-temp').textContent = jsObject.main.temp;
        document.getElementById('temp-min').textContent = jsObject.main.temp_min;
        document.getElementById('temp-max').textContent = jsObject.main.temp_max;
        document.getElementById('current-humidity').textContent = jsObject.main.humidity;
        document.getElementById('wind-speed').textContent = jsObject.wind.speed;
 
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    let wind.temp = jsObject.main.temp; 
-    let wind.windspeed = jsObject.wind.speed;
+       let windChill='N/A';
+       let x;
+          
+       if ((temp >50) || (windspeed <= 3)) {
+   
+           windChill = 'N/A';
 
+         }  else {   
+            x = (35.74 + (0.6215 * temp) - (35.75 * (Math.pow(windspeed, .16))) + 
+            (0.4275 * temp * (Math.pow(windspeed, .16))))
+            windChill = Math.round(x);
+           }
+
+        document.querySelector('#windchill').textContent=windChill;
   });
 
-//the rest is to show me how to call the data from the api
-  
-
-     // console.log(jsObject);    this was used to test the url and it is good
-  //    let currenttemp = document.createElement('p');
-     document.getElementById('current-temp').textContent = jsObject.main.temp;
-    
-
-const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-const desc = jsObject.weather[0].description;  // note how we reference the weather array
-document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc);
-});
