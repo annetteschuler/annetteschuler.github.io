2b6731356aa59720e5a19d7ff810c3c6c1b88cc4
annetteschuler.github.io <annetteschuler1964@gmail.com>
2021-03-13 11:52:34 -0600

    save

--- lesson10/lesson10.js/weatherapi.js
+++ lesson10/lesson10.js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=b512137a92df2e6909b0861d6839b12a';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=b512137a92df2e6909b0861d6839b12a';
 
 fetch(apiURL)
   .then((response) => response.json())
@@ -9,7 +9,7 @@ fetch(apiURL)
        document.getElementById('current-temp').textContent = jsObject.main.temp;
       
 
-const imagesrc = 'http://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
 const desc = jsObject.weather[0].description;  // note how we reference the weather array
 document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
 document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
