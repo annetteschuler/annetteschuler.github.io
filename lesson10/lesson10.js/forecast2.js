abc79dac20ea7735675a9fc87ab4de1d159e3f40
annetteschuler.github.io <annetteschuler1964@gmail.com>
2021-03-13 11:27:24 -0600

    save

--- lesson10/lesson10.js/forecast2.js
+++ lesson10/lesson10.js/forecast2.js
@@ -1,29 +1,48 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=b512137a92df2e6909b0861d6839b12a';
+const apiURL2 = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=b512137a92df2e6909b0861d6839b12a';
 
-console.log(apiURL);
+console.log(apiURL2);
 
-fetch(apiURL)
+fetch(apiURL2)
     .then((response) =>response.json())
-    .then((jsObject) =>) {
+    .then((jsObject) => {
 
-        let day = 0;
-        const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        //reduce the list array to the five forecsts .... from 40
-        //feed the filter a condition... 18:00:00 andgoogle sub
-        includes 1800
-        filter by so...filter it to only 1600 hours??
-        let jsobject.list
-
-
-        theFive.foreEach ( x=> )
-        let d = newdate(x.dt_txt)''
+   
+        let d = 0;
+        let date = new Date();
+        let currentday = date.getDay();
         
-        //loop through each of the forecast days
-        let d = new Date(jsObject.list[4].dt_text);
-        document.getElementById(`dayofweek${day+1}`).textContent = dayofWeek[d.getDay()];
-        document.getElementById(`forecast${day+1}`).textContent = x.main.temp;
 
-        document.getElementById(`forecast${day+1}`)
+        const dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed'];
+             //  document.getElementById(`dayofweek${d+1}`).textContent = dayofWeek[d.getDay()];
+        // document.getElementById(`dayofweek${day+1}`).textContent = dayofWeek[d.getDay()];
+         document.querySelector('#dayofweek1').textContent = dayOfWeek[currentday];
+         document.querySelector('#dayofweek2').textContent = dayOfWeek[currentday + 1];
+         document.querySelector('#dayofweek3').textContent = dayOfWeek[currentday + 2];
+         document.querySelector('#dayofweek4').textContent = dayOfWeek[currentday + 3];
+         document.querySelector('#dayofweek5').textContent = dayOfWeek[currentday + 4];
+    
+    
+        //reduce the list array to the five forecasts .... from 40
+        //feed the filter a condition... 18:00:00 andgoogle sub
+        // build an array that includes only the 1800
+
+
+         const forecast = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));  //filter 1800       
+         let i = 0;
+         forecast.forEach(x => {
+
+            let imagesrc = 'https://openweathermap.org/img/w/' + x.weather[0].icon + '.png'; 
+            let desc = x.weather[0].description;
+
+            // document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+            document.getElementById(`icon${i + 1}`).setAttribute('src', imagesrc);  // get icon
+            document.getElementById(`icon${i + 1}`).setAttribute('alt', desc);      //get alt
+            document.getElementById(`forecast${i + 1}`).textContent = Math.round(x.main.temp); //get temp
+
+            i++;
+         });  
+
+          
     
     });
-    
+      
